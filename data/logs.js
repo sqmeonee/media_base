8900e1d2566b6d1f8db499d1a4236f49215c809b
sqmeonee <ewefwef31@gmail.com>
2023-11-02 16:09:10 -0400

    adding files

--- data/logs.js
+++
@@ -1,8 +0,0 @@
-const SavedLog = require('./models/log');
-
-module.exports = new class {
-  async get(id) {
-    return await SavedLog.findById(id)
-      || await new SavedLog({ _id: id }).save();
-  }
-}
--- data/models/log.js
+++
@@ -1,6 +0,0 @@
-const { model } = require('mongoose');
-
-module.exports = model('log', {
-  _id: String,
-  changes: { type: Array, default: [] }
-});
